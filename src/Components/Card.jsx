b3effe92a1955fe8beaf65f416155f9d6dfb9f3c
afg2002 <afghanekapangestu@gmail.com>
2023-02-28 03:40:44 +0700

    Lagi progress feature login.

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 function Card(props) {
@@ -6,24 +7,27 @@ function Card(props) {
     const croppedArticleBody = data.article_body.substring(0,80).substring(0, data.article_body.substring(0, 80).lastIndexOf(' ')) +
     '...'
     return (
-        <div className="card w-96 bg-base-100 shadow-xl h-full">
+        <div className="card md:w-72 lg:xl:w-10/12 bg-base-100 shadow-xl h-full">
             <figure><img src={data.img} alt="img_article" className='w-full h-56' /></figure>
             <div className="card-body">
                 <div className="card-actions justify-start">
-                <div className="badge badge-outline">{data.category}</div>
+                {data.category === data.category && <Link to={`/c/${data.category.toLowerCase()}`}>
+                    <div className="badge badge-outline">{data.category}</div>
+                </Link>}
+                
                 </div>
                 <h2 className="card-title">
-                {data.article_title}
+                    <Link to={"/"}>{data.article_title}</Link>
                 </h2>
                 <p>{croppedArticleBody}</p>
                 <div className="card-actions justify-end">
                 <div className="author">
                     {data.users.name}
                     <div className="avatar placeholder ml-2">
-                    <div className="bg-neutral-focus text-neutral-content rounded-full w-8">
-                        <span className="text-xs">AA</span>
-                    </div>
-                    </div>
+                            <div className="bg-neutral-focus text-neutral-content rounded-full w-8">
+                                <span className="text-xs">AA</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import ButtonSign from './ButtonSign';
 
 
 const Navbar = () => {
@@ -11,6 +12,9 @@ const Navbar = () => {
     React.useEffect(() => {
       document.querySelector('html').setAttribute('data-theme', theme);
     }, [theme]);
+
+
+
     return (
     <div className="navbar bg-base-100 p-5">
       <div className="navbar-start">
@@ -19,21 +23,15 @@ const Navbar = () => {
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
           </label>
           <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-            <li><a>Item 1</a></li>
-            <li tabIndex={0}>
-              <a className="justify-between">
-                Parent
-                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
-              </a>
-              <ul className="p-2">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
-              </ul>
-            </li>
-            <li><a>Item 3</a></li>
+            <li><Link to="/">All</Link></li>
+            <li><Link to="/c/art">Art</Link></li>
+            <li><Link to="/c/technology">Technology</Link></li>
+            <li><Link to="/c/science">Science</Link></li>
+            <li><Link to="/c/movies">Movies</Link></li>
+            <li><Link to="/c/food">Food</Link></li>
           </ul>
         </div>
-        <a className="btn btn-ghost normal-case text-xl">Blog Mantap</a>
+        <Link to={"/"} className="btn btn-ghost normal-case text-xl">Blog Mantap</Link>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
@@ -55,7 +53,10 @@ const Navbar = () => {
             
           </label>
         </div>
-        <a className="btn">Get started</a>
+        <div className='flex flex-row gap-2'>
+          <label htmlFor="modal-signin" className="btn btn-outline btn-accent">Sign in</label>
+        </div>
+        <ButtonSign/>
       </div>
     </div>
     );
